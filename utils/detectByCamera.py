2ee3d11b408919d012e007241d01bde547cc5d51
ujjwal-anand-0207 <ujjwalsaxena0207@gmail.com>
2023-01-06 16:19:12 +0530

    camera function

---
+++ utils/detectByCamera.py
@@ -0,0 +1,20 @@
+import cv2
+from utils import detect
+
+def detectByCamera(writer):   
+    video = cv2.VideoCapture(0)
+    print('Detecting people...')
+
+    while True:
+        check, frame = video.read()
+
+        frame = detect(frame)
+        if writer is not None:
+            writer.write(frame)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+                break
+
+    video.release()
+    cv2.destroyAllWindows()
